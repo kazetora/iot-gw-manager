a12eefe8c62f693ed4ad87ea906f039d06824178
putra <putra@nakao-lab.org>
2015-08-07 16:52:17 +0900

    do not register bad formatted data (also check data before sending to server as well)

--- routes/events.js
+++ routes/events.js
@@ -19,19 +19,27 @@ router.post('/addEvent', function(req, res, next) {
     var params = req.body;
     var response = {'status':0, message:""};
     var db = req.db;
-    var insert_data = {
-        node_id: params.id,
-        type: (typeof params.type === 'undefined') ? 0 : params.type, // default: 0
-        date_create: new Date(),
-        data: params.data[0]
+    // check data validity
+    if(params.data[0].accel.X !== null && params.data[0].accel.Y !== null && params.data[0].accel.Z !== null) {
+      var insert_data = {
+          node_id: params.id,
+          type: (typeof params.type === 'undefined') ? 0 : params.type, // default: 0
+          date_create: new Date(),
+          accel: params.data[0].accel,
+          lat: params.data[0].gps.latitude,
+          lon: params.data[0].gps.longitude
+      }
+      db.collection('events').insert(insert_data, function(err, result) {
+          if(err) {
+              response.status = 1;
+              response.msg = err;
+          }
+          res.send(response);
+      });
     }
-    db.collection('events').insert(insert_data, function(err, result) {
-        if(err) {
-            response.status = 1;
-            response.msg = err;
-        }
-        res.send(response);
-    });
+    else {
+      res.send(response);
+    }
 });
 
 router.get('/getGeoJson/:id', function(req, res, next) {
@@ -44,7 +52,7 @@ router.get('/getGeoJson/:id', function(req, res, next) {
         //var ret = [];
         //item.foreach
         //res.json(items);
-        var geojson = GeoJSON.parse(items, {Point: ['data.gps.latitude', 'data.gps.longitude']})
+        var geojson = GeoJSON.parse(items, {Point: ['lat', 'lon']})
         console.log(geojson);
         res.send(geojson);
     });
